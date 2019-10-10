5b327c174662645a7139499477b1d77c472e8d08
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-10 18:33:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 def oxford_comma(array)
-  if array.size == 2 
+  if array.length <= 2 
       join_char = " and "
-  elsif array.size == 1 
-    join_char = ","
   else
     last_elem = "and " + array.pop()
     array.push(last_elem)
     join_char = ", "
   end
   new_array = array.join(join_char)
- new_array
 end
+
+puts oxford_comma(["one", "two", "three"])
